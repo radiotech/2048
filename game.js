68df74de63298535f2e81b51e28575d2493505e0
Jennyq998 <jennyhan_wang@outlook.com>
2019-11-02 14:06:16 -0400

    Update game.js

--- game.js
+++ game.js
@@ -376,3 +376,58 @@ $(document).keydown(function(event) {
   }
   update();
 });
+
+$("#game-board")[0].addEventListener("touchstart", handleTouchStart, false);
+$("#game-board")[0].addEventListener("touchmove", handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || evt.originalEvent.touches // browser API
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      /* left swipe */
+      game.move("left");
+    } else {
+      /* right swipe */
+      game.move("right");
+    }
+  } else {
+    if (yDiff > 0) {
+      /* up swipe */
+      game.move("up");
+    } else {
+      /* down swipe */
+      game.move("down");
+    }
+  }
+
+  update();
+
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
